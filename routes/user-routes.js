0220a3c70e164229adae9077f97a845f7149c783
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2023-11-01 14:58:22 +0100

    Added express validator to check if credentials are met with signup

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
+const { check } = require("express-validator");
 
-const usersController = require('../controllers/users-controllers');
+const usersController = require("../controllers/users-controllers");
 
 const router = express.Router();
 
-router.post('/signup', usersController.signup);
-router.post('/login', usersController.login);
+router.post("/signup", usersController.signup, [
+  check("name").not().isEmpty(),
+  check("email").normalizeEmail().isEmail(),
+  check("password").isLength({ min: 6 }),
+]);
+router.post("/login", usersController.login);
 
 module.exports = router;
