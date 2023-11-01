cb59b4085ff15f035435dbb9702101a61d70bf23
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2023-11-01 12:56:52 +0100

    Added login authentication

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -85,4 +85,69 @@ const signup = async (req, res, next) => {
     .json(responseObj);
 };
 
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!existingUser) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      403
+    );
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      'Could not log you in, please check your credentials and try again.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      401
+    );
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      process.env.SECRET_TOKEN,
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  res.json({
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token
+  });
+}
+
 exports.signup = signup;
+exports.login = login;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,5 +4,6 @@ const usersController = require('../controllers/users-controllers');
 const router = express.Router();
 
 router.post('/signup', usersController.signup);
+router.post('/login', usersController.login);
 
 module.exports = router;
